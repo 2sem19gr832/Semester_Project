7717133cc3c56ba9bd776109b3a76510d039b040
martin.jensen37 <martin.jensen37@gmail.com>
2019-05-27 20:03:53 +0200

    Minor changes

--- ReferencesMPC.m
+++ ReferencesMPC.m
@@ -18,6 +18,7 @@
 x = (0:0.1:47.9);
 y = [zeros(1, 10), sind(1:360)*18, sind(1:90)*18, ones(1, 20)*18];
 
+
 for t = 2:480
     deltax(t) = x(t)-x(t-1);
     deltay(t) = y(t)-y(t-1);
@@ -31,8 +32,10 @@ plot(x, y)
 title('Reference Trajectory')
 ylabel('Position [m]')
 xlabel('Position [m]')
+axis([-2 20 -11 11])
 subplot(2,1,2)
-plot(theta_rad(1, 1:470)*180/pi)
+plot(trajectory_angle(1, :)*180/pi)
+axis([-10 400 0 400])
 title('Reference Yaw Angle')
 ylabel('Angle [deg]')
 xlabel('Time Step')
@@ -55,7 +58,9 @@ xlabel('Time Step')
 %xlabel('Time Step')
 %%
 %subplot(3, 1, 3)
-plot(position(:, 1)/15.5031, position(:, 2  ))
+figure(1)
+subplot(2, 1, 1)
+plot(position(:, 1)/15.5031, position(:, 2))
 hold on
 plot(x, y)
 title('Comparison of Reference and model output (Hu = 3, Hp = 10)')
@@ -63,4 +68,33 @@ xlabel('Position [m]')
 ylabel('Position [m]')
 legend('Model Output', 'Reference')
 hold off
+subplot(2, 1, 2)
+fixedsize= [position(:, 1)/15.5031, position(:, 2)]';
+errorx = position(1:480, 1)- x(1, :)';
+errory = position(1:480, 2) - y(1, :)';
+dist = sqrt(errorx.^2 + errory.^2);
+%tx = finddelay(position(1:480, 1), x(1, :));
+%ty = finddelay(position(1:480, 2), y(1, :));
+plot(errorx, errory)
+title('Positional Error')
+ylabel('Position [m]')
+xlabel('Time step')
+%%
+plot(Openloopstep(1:10:480, 1))
+hold on
+plot(Openloopstep(1:10:480, 2))
+xlabel('Position [m]')
+ylabel('Position [m]')
+title('Comparison of Reference Position')
+legend('Reference', 'Model Output')
+
+error = Openloopstep(1:10:480, 1)-Openloopstep(1:10:480, 2);
+
+meanerror = mean(abs(error))
+
+hold off
+%plot((1:0.1:49.9), Openloopstep(:, 1))
+%hold on 
+%plot((1:0.1:49.9), Openloopstep(:, 2))
+%magnifyOnFigure;
     
